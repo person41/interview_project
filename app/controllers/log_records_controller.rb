c31fedd49178136509d6b3c5b2482cd5dc1fbe6e
Person 41 <person4.1@protonmail.com>
2019-09-25 20:31:31 -0700

    [CH-8] Join logs and populations

--- app/controllers/log_records_controller.rb
+++ app/controllers/log_records_controller.rb
@@ -1,5 +1,11 @@
 class LogRecordsController < ApplicationController
   def index
     @log_records = LogRecord.all
+    @requests_by_year = Population
+                          .select("populations.year, COUNT(log_records.year) AS request_count")
+                          .joins("LEFT JOIN log_records ON log_records.year = populations.year AND log_records.exact = TRUE")
+                          .group("populations.year")
+                          .order("request_count DESC")
+
   end
 end
